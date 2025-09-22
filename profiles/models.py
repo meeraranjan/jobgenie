f37813e0ff647fa66d4e8343b4cd7901da3f94eb
Meera Ranjan <ranjan.meera@kabam.ai>
2025-09-22 18:53:25 -0400

    Made modifications to fields in JobSeekerProfile

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+@receiver(post_save, sender=User)
+def create_jobseeker_profile(sender, instance, created, **kwargs):
+    if created:
+        JobSeekerProfile.objects.create(user=instance)
+        
 class JobSeekerProfile(models.Model):
+    first_name = models.CharField(max_length=255, default='Unknown')
+    last_name = models.CharField(max_length=255, default='Unknown')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
     skills = models.TextField()
@@ -11,5 +20,5 @@ class JobSeekerProfile(models.Model):
     is_public = models.BooleanField(default=True)  # privacy
 
     def __str__(self):
-        return f"{self.user.username} - {self.headline}"
+        return f"{self.username} - {self.headline}"
 

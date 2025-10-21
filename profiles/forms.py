03c671f1166842fbbe4a54bf9b3e1b4068b31e87
meeraranjan <153428812+meeraranjan@users.noreply.github.com>
2025-10-21 12:58:24 -0400

    Added address fields to JobSeeker model

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,7 +4,8 @@ from .models import JobSeekerProfile
 class JobSeekerProfileForm(forms.ModelForm):
     class Meta:
         model = JobSeekerProfile
-        fields = ['first_name', 'last_name','headline', 'skills', 'education', 'work_experience', 'links', 'is_public']
+        fields = ['first_name', 'last_name','headline', 'skills', 'education', 'work_experience', 'links', 'is_public',
+                   "street", "apartment", "city", "state", "postal_code", "country",]
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
@@ -14,6 +15,13 @@ class JobSeekerProfileForm(forms.ModelForm):
             'work_experience': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'links': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
             'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'street': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '123 Main St'}),
+            'apartment': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apt 4B'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
+            'state': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}),
+            'postal_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'ZIP / Postal Code'}),
+            'country': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}),
+            
         }
         labels = {
             'is_public': 'Make my profile visible to others', 

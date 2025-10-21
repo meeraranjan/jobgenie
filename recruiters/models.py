4177fb253c4960c0ba7970738e51bb47efe8e1c5
meeraranjan <153428812+meeraranjan@users.noreply.github.com>
2025-10-21 13:30:35 -0400

    Removed duplicate code

--- recruiters/models.py
+++ recruiters/models.py
@@ -9,12 +9,3 @@ class Recruiter(models.Model):
     def __str__(self):
         return f"{self.user.username} ({self.company_name})"
 
-
-class Recruiter(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name="recruiter_profile")
-    company_name = models.CharField(max_length=255, default="Unknown Company")
-    position = models.CharField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.user.username} ({self.company_name})"
